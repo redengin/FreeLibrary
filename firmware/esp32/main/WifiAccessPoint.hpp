b3020f999c05363bba0a3955adf7a515c1a933f2
redengin <sholstei@yahoo.com>
2024-11-19 21:58:06 -0500

    in work

--- firmware/esp32/main/WifiAccessPoint.hpp
+++ firmware/esp32/main/WifiAccessPoint.hpp
@@ -10,7 +10,7 @@ public:
     /// @brief Creates the Open Wifi Access Point
     /// @post call publish()
     /// @param ssid 
-    WifiAccessPoint(const std::string ssid = "LittleFreeLibrary");
+    WifiAccessPoint(const std::string ssid = "Little Free Library");
 
     /// @brief change the published SSID
     void setSsid(const std::string ssid);
