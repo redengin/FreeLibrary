c30b6e8db17639cec00da9313c04bb30250a56ec
redengin <sholstei@yahoo.com>
2024-11-28 03:43:05 -0500

    renaming to identify wildcard

--- firmware/esp32/main/rest/catalog.cpp
+++ firmware/esp32/main/rest/catalog.cpp
@@ -1,6 +1,6 @@
 #include "catalog.hpp"
 
-constexpr std::string uri{"/catalog/*"};
+constexpr std::string uri_wildcard{"/catalog/*"};
 
 extern "C" esp_err_t GET(httpd_req_t*);
 extern "C" esp_err_t PUT(httpd_req_t*);
@@ -20,7 +20,7 @@ void rest::registerCatalog(WebServer& webserver, Catalog& catalog)
 
     webserver.registerUriHandler(
         httpd_uri_t{
-            .uri = uri.c_str(),
+            .uri = uri_wildcard.c_str(),
             .method = HTTP_GET,
             .handler = GET,
             .user_ctx = &context
@@ -28,7 +28,7 @@ void rest::registerCatalog(WebServer& webserver, Catalog& catalog)
     );
     webserver.registerUriHandler(
         httpd_uri_t{
-            .uri = uri.c_str(),
+            .uri = uri_wildcard.c_str(),
             .method = HTTP_PUT,
             .handler = PUT,
             .user_ctx = &context
@@ -36,7 +36,7 @@ void rest::registerCatalog(WebServer& webserver, Catalog& catalog)
     );
     webserver.registerUriHandler(
         httpd_uri_t{
-            .uri = uri.c_str(),
+            .uri = uri_wildcard.c_str(),
             .method = HTTP_DELETE,
             .handler = DELETE,
             .user_ctx = &context
@@ -44,10 +44,10 @@ void rest::registerCatalog(WebServer& webserver, Catalog& catalog)
     );
 }
 
-static std::string relativeUri(const char* const requestUri)
+static std::string catalogUri(const char* const requestUri)
 {
     // omit the base uri
-    return std::string(requestUri + uri.length() - sizeof('*'));
+    return std::string(requestUri + uri_wildcard.length() - sizeof('*'));
 }
 
 enum class UriType {
@@ -80,7 +80,7 @@ UriType uriType(const std::string uri)
 esp_err_t GET(httpd_req_t* const request)
 {
     // const Context& context = reinterpret_cast<const Context&>(request->user_ctx);
-    const auto uri = relativeUri(request->uri);
+    const auto uri = catalogUri(request->uri);
     switch(uriType(uri))
     {
         // TODO
@@ -91,7 +91,7 @@ esp_err_t GET(httpd_req_t* const request)
 esp_err_t PUT(httpd_req_t* const request)
 {
     // const Context& context = reinterpret_cast<Context&>(request->user_ctx);
-    const auto uri = relativeUri(request->uri);
+    const auto uri = catalogUri(request->uri);
     switch(uriType(uri))
     {
         // TODO
@@ -102,7 +102,7 @@ esp_err_t PUT(httpd_req_t* const request)
 esp_err_t DELETE(httpd_req_t* const request)
 {
     // const Context& context = reinterpret_cast<Context&>(request->user_ctx);
-    const auto uri = relativeUri(request->uri);
+    const auto uri = catalogUri(request->uri);
     switch(uriType(uri))
     {
         // TODO
