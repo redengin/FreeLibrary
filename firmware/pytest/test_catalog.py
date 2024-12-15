1798f298f3a852e7d0b9f105638105b300e9660d
redengin <sholstei@yahoo.com>
2024-12-15 00:01:24 -0500

    in work

---
+++ firmware/pytest/test_catalog.py
@@ -0,0 +1,30 @@
+import requests
+
+catalog_base_url = "http://192.168.4.1/catalog/"
+FILE_NAME = "testfile"
+FILE_SIZE = 100000
+FILE_PAYLOAD = "X" * FILE_SIZE
+FILE_TITLE = "this is a testfile"
+FILE_ICON = "X" * FILE_SIZE
+
+
+def test_catalog():
+    # create a new file
+    test_url = catalog_base_url + FILE_NAME
+    response = requests.put(test_url, data=FILE_PAYLOAD)
+    assert response.status_code == 200
+
+    # retrieve that file
+    response = requests.get(test_url)
+    assert 200 == response.status_code
+    assert FILE_SIZE == len(response.content)
+    assert FILE_PAYLOAD == response.content
+
+    # remove that file
+    response = requests.delete(test_url)
+    assert response.status_code == 200
+
+
+
+
+
